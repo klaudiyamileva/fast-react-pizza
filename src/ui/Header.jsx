125b1eb31f6f9cb4b48473a42828087d0fc382c6
klaudiyamileva <klaudiyamileva@gmail.com>
2023-09-07 15:55:22 +0300

    building the App Layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+    return (
+        <header>
+            <Link to='/'>Fast React Pizza Co.</Link>
+            <p>Klaudiya</p>
+        </header>
+    )
+}
+
+export default Header
