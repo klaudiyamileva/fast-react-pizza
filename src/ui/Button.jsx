55e2a2c1d0ad38d0a6e46e85b2ea1d4ba68c4914
klaudiyamileva <klaudiyamileva@gmail.com>
2023-09-14 16:40:03 +0300

    styled cart

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -4,9 +4,10 @@ function Button({ children, disabled, to, type }) {
   const base =
     "transition-color inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
   const styles = {
-    primary: base + " px-4 py-3 md:px-6 md:py-4",
+    primary: base + " px-4 py-3",
     small: base + " px-4 py-2 text-sm",
-    // small: base + " px-4 py-2 md:px-5 md:py-2.5",
+    secondary:
+      "transition-color px-4 py-2.5 inline-block rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 duration-300 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed",
   };
 
   if (to)
